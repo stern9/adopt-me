1e92e26082ad1fd073a2fca1c08c05d2802451bb
astern3 <avram.stern@digitas.com>
2020-12-28 21:34:31 -0600

    Adding express and server side rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState, lazy, Suspense } from "react";
-import { render } from "react-dom";
-import { Router } from "@reach/router";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
 
-// code splitting
-const Details = lazy(() => import("./Details"));
-const SearchParams = lazy(() => import("./SearchParams"));
+// // code splitting
+// const Details = lazy(() => import("./Details"));
+// const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const themeHook = useState("darkBlue");
@@ -15,16 +17,16 @@ const App = () => {
       <ThemeContext.Provider value={themeHook}>
         <div>
           <NavBar />
-          <Suspense fallback={<h1>loading route..</h1>}>
-            <Router>
-              <SearchParams path="/" />
-              <Details path="/details/:id" />
-            </Router>
-          </Suspense>
+          {/* <Suspense fallback={<h1>loading route..</h1>}> */}
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+          {/* </Suspense> */}
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
